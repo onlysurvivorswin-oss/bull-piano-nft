23e44684e8a86fdc700b709c12970c0c6c97b68d
onlysurvivorswin-oss <onlysurvivorswin@gmail.com>
2025-09-25 13:13:00 +1000

    Add client/src/App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import NFTViewer from "@/pages/nft-viewer";
+
+function Router() {
+  return (
+    <Switch>
+      {/* NFT Viewer page */}
+      <Route path="/" component={NFTViewer} />
+      {/* Fallback to 404 */}
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
